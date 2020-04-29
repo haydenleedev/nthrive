7dabfaf07095a1d38239fd77dbd0960a2b1370cc
Alli Bermudez Huff <40130890+AlliHuff@users.noreply.github.com>
2020-04-29 15:10:30 -0400

    Deleted all assets. Added some HTML pages and some used assets

--- _Assets/js/c-notification.js
+++
@@ -1,1 +0,0 @@
-$('.accept-close').click(function () { $('body').addClass("notification-close"); $.cookie('visited', 'yes', { expires: 7 }); }); $(document).ready(function () { var visited = $.cookie('visited'); if (visited == 'yes') { $('body').removeClass("notification-close").addClass("notification-close"); return false; } else { $('body').removeClass("notification-close"); } });
